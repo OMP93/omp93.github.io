3050069ddaade90308f8145f16bf008177a5a3ad
Oleg Lysenko <oleg.lysenko@corp.mailfire.io>
2017-10-26 15:54:33 +0300

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -10,11 +10,11 @@ firebase.messaging();
 
 self.addEventListener('push', function (event) {
     const options = JSON.parse(event.data.text());
-    options.data = options;
+    options.notification.data = options;
     console.log(options);
     // options.data = options;
     event.waitUntil(
-        self.registration.showNotification(options.notification.title, options)
+        self.registration.showNotification(options.notification.title, options.notification)
     );
     // // Track open
     // fetch('https://api.mailfire.io/v1/webpush/show/' + options.id, {
