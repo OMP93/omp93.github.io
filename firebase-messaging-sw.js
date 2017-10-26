05c8753a562ffe0b9c200eb9f0cc19964bc3c912
Oleg Lysenko <oleg.lysenko@corp.mailfire.io>
2017-10-26 15:02:15 +0300

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -8,7 +8,15 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-messaging.onMessage(function(payload) {
-    console.log("Message received. ", payload);
-    // ...
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
 });
