34757696505ff4d8a7562ab5864aa7c91ed71cc4
Oleg Lysenko <oleg.lysenko@corp.mailfire.io>
2017-10-26 15:05:44 +0300

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,34 +6,9 @@ firebase.initializeApp({
     messagingSenderId: '530470765559'
 });
 
-firebase.messaging();
+const messaging = firebase.messaging();
 
-self.addEventListener('push', function (event) {
-    const options = JSON.parse(event.data.text());
-    console.log(options);
-    // options.data = options;
-    event.waitUntil(
-        self.registration.showNotification(options.notification.title, options.notification)
-    );
-    // // Track open
-    // fetch('https://api.mailfire.io/v1/webpush/show/' + options.id, {
-    //     method: "post"
-    // });
-});
-
-self.addEventListener('notificationclick', function (event) {
-    // event.notification.close();
-    console.log(event);
-    // // Show page
-    // event.waitUntil(
-    //     clients.matchAll({
-    //         type: "window"
-    //     }).then(function () {
-    //         return clients.openWindow(event.notification.data.url);
-    //     })
-    // );
-    // // Track click
-    // fetch('https://api.mailfire.io/v1/webpush/click/' + event.notification.data.id, {
-    //     method: "post"
-    // });
+messaging.onMessage(function(payload) {
+    console.log("Message received. ", payload);
+    // ...
 });
